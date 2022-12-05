83f3641d4d7cfe916e12708b736e2a5345bb0c19
Anton Erofeev <69521655+gordjelin@users.noreply.github.com>
2022-12-05 17:32:27 +0200

    Update api.py

--- api.py
+++ api.py
@@ -47,8 +47,7 @@ class PetFriends:
             })
         headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
 
-        #res = requests.post(self.base_url + 'api/pets', headers=headers, data=data)
-        res = requests.post(self.base_url + '/api/pets', headers=headers, data=data)
+        res = requests.post(self.base_url + 'api/pets', headers=headers, data=data)
         status = res.status_code
         try:
             result = res.json()
